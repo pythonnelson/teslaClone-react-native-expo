211ff36f902bcf773c58c49d6fb9502f9303a298
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2022-10-07 10:44:28 +0100

    Completed smooth scrolling and cars list

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem 
-        name={"Model X"} 
-        tagline={"Order Online For"}
-        taglineCTA={"Touchless Delivery"}
-        image={require('./assets/images/ModelX.jpeg')} 
-      />
-
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/CarsList/cars.js
@@ -0,0 +1,27 @@
+export default [{
+    name: 'Model S',
+    tagline: 'Order Online From',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelS.jpeg'),
+},
+
+{
+    name: 'Model 3',
+    tagline: 'Order Online From',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Model3.jpeg'),
+},
+
+{
+    name: 'Model X',
+    tagline: 'Order Online From',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelX.jpeg'),
+},
+
+{
+    name: 'Model Y',
+    tagline: 'Order Online From',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelY.jpeg'),
+}];
